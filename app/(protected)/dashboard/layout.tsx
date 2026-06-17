817a5b617677077076e2ee31ff5016aa79fbaf12
itz-devilx <intekhab118211989@gmail.com>
2026-06-17 19:42:43 +0530

    feat: implement dashboard layout and navigation components with user authentication

---
+++ app/(protected)/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import { requireAuth } from "@/features/auth/actions"
+import {DashboardShell} from "@/features/dashboard/components/dashboard-shell";
+
+export default async function DashboardLayout({children}: {children: React.ReactNode}) {
+
+  const session = await requireAuth();
+
+  return (
+    <DashboardShell user={session?.user} plan={"pro"}>
+      {children}
+    </DashboardShell>
+  )
+}
